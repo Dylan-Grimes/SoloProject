3a1be7ba657d05e4661e92ae43f2c2b528f29434
dylan <dylan.grimes@academytrainee.com>
2019-09-25 13:45:55 +0100

    added attribute for each row

--- FrontEnd/js/read.js
+++ FrontEnd/js/read.js
@@ -41,6 +41,11 @@ function inTable(data) {
     for (let team of data) {
         contInner = document.createElement("td");
         contInner.innerHTML = team;
+        for (let key of team) {
+        let tagName = document.createAttribute("name");
+        tagName.value = key;
+        contInner.setAttributeNode(tagName);
+        }
         container.appendChild(contInner);
     }
 
