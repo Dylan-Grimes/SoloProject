dd9c034dc3d438c4ce5a10ccc12577c94b18a6b3
dylan <dylan.grimes@academytrainee.com>
2019-09-25 17:02:32 +0100

    changed loop in read for data input 16

--- FrontEnd/js/read.js
+++ FrontEnd/js/read.js
@@ -34,7 +34,7 @@ function readTeams() {
 function inTable(data) {
     let tableBody = document.getElementById("tableBody");
     let contInner;
-    console.log(data, data.value);
+    console.log(data, data.id);
 
     let container = document.createElement("tr");
     tableBody.appendChild(container);
