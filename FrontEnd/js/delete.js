f8f57574a90295e2eeaba8c1a549ba0022659e5e
dylan <dylan.grimes@academytrainee.com>
2019-09-26 13:53:43 +0100

    fixed players page and added delet eplayer

--- FrontEnd/js/delete.js
+++ FrontEnd/js/delete.js
@@ -16,3 +16,22 @@ function deleteTeam() {
         
 
 }
+
+function deletePlayer() {
+
+    var input = document.getElementById("idToDeleteP").value;
+
+    console.log(input);
+
+    makeRequest("http://34.89.70.8:9000/players/", input , type = "DELETE")
+        .then((data) => {
+            console.log("Deleted" + data);
+            window.location.href = window.location.href
+        })
+        .catch((data) => {
+            console.log("It failed!" + data);
+        })
+
+        
+
+}
