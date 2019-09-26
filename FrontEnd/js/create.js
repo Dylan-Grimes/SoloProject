7e4795f29d8e3e88c0614193e8cd94bfa1ddc616
dylan <dylan.grimes@academytrainee.com>
2019-09-26 14:37:11 +0100

    dsaw

--- FrontEnd/js/create.js
+++ FrontEnd/js/create.js
@@ -29,3 +29,33 @@ function addTeam(data) {
 
     return false;
 }
+
+function addPlayer(data) {
+
+    for (let element of data) {
+        if (element.name) {
+            formDataObj[element.name] = element.value;
+        }
+    }
+
+    makeRequest("http://34.89.70.8:9000/players", formDataObj, type = "POST")
+        .then((data) => {
+            console.log("it Worked!" + data);
+
+            $('#exampleModalCenter').modal('hide');
+            $('.modal').on('hidden.bs.modal', function () {
+                $(this).find('form')[0].reset();
+            });
+
+            window.location.href = window.location.href;
+        })
+        .catch((data) => {
+            console.log("It failed!" + data);
+        })
+
+    console.log(formDataObj);
+
+
+
+    return false;
+}
