da4075ace71578446ac7cf45f3622562dbd5d2f7
dylan <dylan.grimes@academytrainee.com>
2019-09-26 16:12:38 +0100

    added update player

--- FrontEnd/js/update.js
+++ FrontEnd/js/update.js
@@ -32,3 +32,38 @@ function submitUpdateForm(formData) {
 
     return false;
 }
+
+function submitUpdateFormPlayers(formData) {
+    let keyValues = {};
+    let id = 0;
+
+    for (let element of formData.elements) {
+        if (element.name) {
+            if (element.name === "id") {
+                id = element.value;
+            } else {
+                keyValues[element.name] = element.value;
+            }
+        }
+    }
+
+    console.log("http://34.89.70.8:9000/players/" + id);
+    console.log(keyValues);
+
+    makeRequest("http://34.89.70.8:9000/players/" + id, keyValues, "PUT")
+        .then((data) => {
+            console.log("it Worked!" + data);
+
+            $('#updateFunctionality').modal('hide');
+            $('.modal').on('hidden.bs.modal', function () {
+                $(this).find('form')[0].reset();
+            });
+
+            window.location.href = window.location.href
+        })
+        .catch((data) => {
+            console.log("It failed!" + data);
+        });
+
+    return false;
+}
